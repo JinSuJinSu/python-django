0828860278ef5ab62918448a9dfc687d32974ca1
hjs429 <hjs928@naver.com>
2021-07-16 17:48:13 +0900

    inital commit

---
+++ BoardProject2/boardRoot/noticeboard/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, get_object_or_404
+from noticeboard.models import Notice
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
+
+# Create your views here.
+def index(request):
+    article_list = Notice.objects.all().order_by('-writeDate')
+    content = {'article_list': article_list}
+    return render(request, 'noticeboard/index.html', content)
+
+def write_article(request):
+    return render(request, 'noticeboard/writeArticle.html')
+
+def add_article(request):
+    notice = Notice()
+    notice.title = request.POST['title']
+    notice.content = request.POST['content']
+    notice.writeID = 'jinsujinsu'
+    notice.save()
+    return HttpResponseRedirect(reverse('noticeboard:index'))
+
+def view_article(request, article_id):
+    notice = get_object_or_404(Notice, pk=article_id)
+    return render(request, 'noticeboard/detail.html', {'article':notice})
+
+
+def update_article(request, article_id):
+    notice = Notice.objects.get(id=article_id)
+    # 불법 접근을 막기 위해
+    if request.method == 'POST':
+        notice.title = request.POST['title']
+        notice.content = request.POST['content']
+        notice.writeDate = timezone.datetime.now()
+        notice.save()
+        return HttpResponseRedirect(reverse('noticeboard:view',args={article_id}))
+    else:
+        return render(request, 'noticeboard/detail.html', {'article': notice})
+
+
+def delete_article(request, article_id):
+    notice = Notice.objects.get(id=article_id)
+    notice.delete()
+    return HttpResponseRedirect(reverse('noticeboard:index'))
+
+
+
+
