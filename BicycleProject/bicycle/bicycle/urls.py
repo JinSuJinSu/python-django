955a558a9f06b4a91034537798bf9e76c41b947c
hjs429 <hjs928@naver.com>
2021-09-05 11:00:00 +0900

    add project graph

--- BicycleProject/bicycle/bicycle/urls.py
+++ BicycleProject/bicycle/bicycle/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bicycle_anal.urls')),
 
-]
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- BicycleProject/bicycle/bicycle_anal/views.py
+++ BicycleProject/bicycle/bicycle_anal/views.py
@@ -1,52 +1,37 @@
-from django.shortcuts import render
-import matplotlib.pyplot as plt
-import io
-import urllib, base64
-import pandas as pd
-from . import matplot
+from django.shortcuts import render, get_object_or_404
+
 
 
 
 # Create your views here.
 
+def index(request):
+    return render(request,'bicycle_html/home.html')
 
+def basic_visual(request):
+    return render(request,'bicycle_html/basic_visual.html')
 
-def home(request):
-    plt.plot(range(10))
-    fig = plt.gcf()
+def visual_sum(request):
+    return render(request,'bicycle_html/visual_sum.html')
 
-    buf=io.BytesIO()
-    fig.savefig(buf, format='png')
-    buf.seek(0)
-    string = base64.b64encode(buf.read())
-    uri =  urllib.parse.quote(string)
-    return render(request,'home.html',{'data':uri})
+def visual3(request):
+    return render(request,'bicycle_html/visual3.html')
 
+def visual4(request):
+    return render(request,'bicycle_html/visual4.html')
 
-# def graph1(request):
-#     data = pd.read_csv('./csv_files/연도별 이용건수.csv')
-#     ax = plt.gca()
-#     ax.axes.get_yaxis().set_ticks([])
-#     df2 = data['이용건수']
-#     df2.index = [2017, 2018, 2019, 2020]
-#     df2.plot(kind='bar', grid=False, figsize=(10, 10))
-#     plt.xlabel('연도', fontsize=20)
-#     plt.ylabel('이용건수', fontsize=20)
-#     plt.xticks(fontsize=16, rotation=0)
-#     for i in range(len(df2)):
-#         plt.text(i, df2.iloc[i], int(df2.iloc[i] * 10 ** 7),
-#                  fontsize=20,
-#                  horizontalalignment='center',
-#                  verticalalignment='bottom')
-#
-#     fig = plt.gcf()
-#     buf=io.BytesIO()
-#     fig.savefig(buf, format='png')
-#     print(fig)
-#     buf.seek(0)
-#     string = base64.b64encode(buf.read())
-#     uri =  urllib.parse.quote(string)
-#     return render(request,'home.html',{'data':uri})
+def data_intro(request):
+    return render(request,'bicycle_html/data_intro.html')
+
+def data_process(request):
+    return render(request,'bicycle_html/data_process.html')
+
+def data_anal(request):
+    return render(request,'bicycle_html/data_anal.html')
+
+def data_result(request):
+    return render(request,'bicycle_html/data_result.html')
+
 
 
 
